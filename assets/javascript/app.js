473b1edb164fcb653245f10bc08dc1657c49798c
Kevin Perez <kevinperez996@outlook.com>
2019-09-09 23:05:28 -0400

    Added some Js to display the question onto the DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,38 @@
 
 
+
+
+
+
+
+
+//--------------------QUESTIONS--------------------
+var questions = ["Which car manufacturer is from Germany?", "What is the slogan for BMW?", "Which was the first American auto manufacturer?"]
+
+for (var i = 0; i < questions.length; i++){
+    $("#question").text(questions[i]);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------COUNTER---------------------------
+//Variables to hold values for countdown timer
 var timeKeep;
 var time = 30;
 var countingDown = false;
 $("#clock").text(time);
 
-
+//Function to start the clock 
 function clockStart() {
     if (!countingDown) {
         timeKeep = setInterval(count, 1000);
@@ -19,8 +46,6 @@ function count() {
     if (time === 0){
         stop();
     }
-   
-
 
     $("#clock").text(time);
 };
