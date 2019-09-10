056631ca43c6cdec999781924f761f3adf23a5b2
Kevin Perez <kevinperez996@outlook.com>
2019-09-10 00:35:22 -0400

    Added a bit of logic to replace the questions after user answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,12 +8,63 @@
 
 //--------------------QUESTIONS--------------------
 var questions = ["Which car manufacturer is from Germany?", "What is the slogan for BMW?", "Which was the first American auto manufacturer?"]
+//Choose a random
+//var randQuestion = Math.floor(Math.random ()* questions.length);
 
-for (var i = 0; i < questions.length; i++){
-    $("#question").text(questions[i]);
+
+
+
+var wins = 0;
+var losses = 0;
+
+//-------Q1-------
+function firstQest() {
+
+    $("#question").text(questions[0]);
+    $("#C1").text("Ford");
+    $("#C2").text("Cadilac");
+    $("#C3").text("Toyota");
+    $("#C4").text("BMW");
+
+    $("#C1, #C2, #C3").on("click", function () {
+        losses++;
+        alert ("L");
+        
+    });
+
+    $("#C4").on("click", function () {
+        wins++;
+        alert ("W");
+        secondQuest();
+        
+    });
+  
+};
+console.log(wins);
+firstQest();
+
+function secondQuest() {
+    $("#question").text(questions[1]);
+    $("#C1").text("Drive Fast");
+    $("#C2").text("Ultimate driving Machine");
+    $("#C3").text("Lets go Places");
+    $("#C4").text("Drive further");
+
+    $("#C1, #C3, #C4").on("click", function () {
+        alert("YOu Loose");
+    });
+
+    $("#C2").on("click", function () {
+        alert("YOu Winn");
+        secondQuest();
+    });
 };
 
+function thristQuest() {
 
+    $("#question").text(questions[2]);
+
+};
 
 
 
@@ -43,13 +94,13 @@ function clockStart() {
 
 function count() {
     time--;
-    if (time === 0){
+    if (time === 0) {
         stop();
     }
 
     $("#clock").text(time);
 };
-function stop (){
+function stop() {
     clearInterval(timeKeep);
 }
 
