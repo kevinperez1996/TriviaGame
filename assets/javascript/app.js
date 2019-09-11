e893c9dbf900067920e8860e42c6d0c86cc7f3e3
Kevin Perez <kevinperez996@outlook.com>
2019-09-11 01:30:36 -0400

    changed style of game to show all questions on the page at once, with a single timer. Began implementing radio inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,64 +7,24 @@
 
 
 //--------------------QUESTIONS--------------------
-var questions = ["Which car manufacturer is from Germany?", "What is the slogan for BMW?", "Which was the first American auto manufacturer?"]
-//Choose a random
-//var randQuestion = Math.floor(Math.random ()* questions.length);
-
-
-
-
+var questions = ["Which car manufacturer is from Germany?", "What is the slogan for BMW?", "Which was the first American auto manufacturer?", 
+"What car manufacturer currently holds the top speed record?"];
 var wins = 0;
 var losses = 0;
 
-//-------Q1-------
-function firstQest() {
+//-------QUESTIONS-------//
 
-    $("#question").text(questions[0]);
-    $("#C1").text("Ford");
-    $("#C2").text("Cadilac");
-    $("#C3").text("Toyota");
-    $("#C4").text("BMW");
+$("#question1").text(questions[0]);
 
-    $("#C1, #C2, #C3").on("click", function () {
-        losses++;
-        alert ("L");
-        
-    });
+$("#question2").text(questions[1]);
 
-    $("#C4").on("click", function () {
-        wins++;
-        alert ("W");
-        secondQuest();
-        
-    });
-  
-};
-console.log(wins);
-firstQest();
+$("#question3").text(questions[2]);
 
-function secondQuest() {
-    $("#question").text(questions[1]);
-    $("#C1").text("Drive Fast");
-    $("#C2").text("Ultimate driving Machine");
-    $("#C3").text("Lets go Places");
-    $("#C4").text("Drive further");
+$("#question4").text(questions[3]);
 
-    $("#C1, #C3, #C4").on("click", function () {
-        alert("YOu Loose");
-    });
 
-    $("#C2").on("click", function () {
-        alert("YOu Winn");
-        secondQuest();
-    });
-};
 
-function thristQuest() {
-
-    $("#question").text(questions[2]);
-
-};
+   
 
 
 
@@ -76,12 +36,17 @@ function thristQuest() {
 
 
 
-//----------------COUNTER---------------------------
+
+//----------------COUNTER----------------------//
 //Variables to hold values for countdown timer
 var timeKeep;
 var time = 30;
 var countingDown = false;
 $("#clock").text(time);
+
+function clockReset() {
+    time = 31;
+};
 
 //Function to start the clock 
 function clockStart() {
