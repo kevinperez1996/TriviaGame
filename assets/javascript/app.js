1b27549a97bd6a9a71dca4d558aa40f7b030aed5
Kevin Perez <kevinperez996@outlook.com>
2019-09-11 23:08:55 -0400

    fixed a small glitch where the timer would start before the start button was clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,122 +1,128 @@
 
-$( document ).ready(function() {
-//--------------------QUESTIONS--------------------//
-var questions = ["Which car manufacturer is from Germany?", `What car manufacturer's slogan is "The ultimate driving machine"?`, "Which car manufacturer is from France?",
-    "What car manufacturer currently holds the top speed record?"];
-
-//-------QUESTIONS-------//
-
-$("#question1").text(questions[0]);
-
-$("#question2").text(questions[1]);
-
-$("#question3").text(questions[2]);
-
-$("#question4").text(questions[3]);
-
-
-var correct = 0;
-
-$("#submit").on("click", function (event) {
-
-    event.preventDefault();
-    $("#smallContainer").hide();
+$(document).ready(function () {
     clockStop();
+    //--------------------QUESTIONS--------------------//
+    var questions = ["Which car manufacturer is from Germany?", `What car manufacturer's slogan is "The ultimate driving machine"?`, "Which car manufacturer is from France?",
+        "What car manufacturer currently holds the top speed record?"];
+
+    //-------QUESTIONS-------//
+
+    $("#question1").text(questions[0]);
+
+    $("#question2").text(questions[1]);
+
+    $("#question3").text(questions[2]);
+
+    $("#question4").text(questions[3]);
+
+
+    var correct = 0;
+
+    $("#submit").on("click", function (event) {
+
+        event.preventDefault();
+        $("#smallContainer").hide();
+        //clockStop();
+        gameOver();
+    });
+
+    $("#choice").on("click", function () {
+
+        var a1 = document.trivia.q1.value
+        var a2 = document.trivia.q2.value
+        var a3 = document.trivia.q3.value
+        var a4 = document.trivia.q4.value
+        console.log(a1);
+        console.log(a2);
+        console.log(a3);
+        console.log(a4);
+
+        if (a1 === "Audi") {
+            correct++;
+        }
+
+        if (a2 === "BMW") {
+            correct++;
+        }
+
+        if (a3 === "Peugeot") {
+            correct++;
+        }
+
+        if (a4 === "Bugatti") {
+            correct++;
+        }
+
+        console.log("correct: " + correct);
+
+
+
+
+    })
+
+    //---------------GAMEOVER--------------------//
+    var imageURL;
+
+    function gameOver() {
+
+        if (correct <= 1) {
+            imageURL = "https://media.giphy.com/media/jUwpNzg9IcyrK/giphy.gif";
+        }
+        else if (correct <= 2) {
+            imageURL = "https://media.giphy.com/media/9xijGdDIMovchalhxN/giphy.gif";
+        }
+        else if (correct <= 3) {
+            imageURL = "https://media.giphy.com/media/Lcn0yF1RcLANG/giphy.gif";
+        }
+        else if (correct = 4) {
+            imageURL = "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif";
+        }
+
+        $("#allDone").text("ALL DONE!!");
+        $("#pointCounter").text("You got " + correct + " of the 10 questions correct.");
+        $("#doneImg").prepend($("<img>").attr("src", imageURL));
+
+
+    };
+
+    $("#start").on("click", function () {
+        $("#smallContainer").show();
+        $("#start").hide();
+        clockStart();
+    })
+
+
+
+    //----------------COUNTER----------------------//
+    //Variables to hold values for countdown timer
+    var timeKeep;
+    var time;
+    var countingDown = false;
     
-    //Answer Key/Checker
     
-    var a1 = document.trivia.q1.value
-    var a2 = document.trivia.q2.value
-    var a3 = document.trivia.q3.value
-    var a4 = document.trivia.q4.value
-    console.log(a1);
-    console.log(a2);
-    console.log(a3);
-    console.log(a4);
     
-    if (a1 === "Audi") {
-        correct++;
-    }
+    //Function to start the clock 
+    function clockStart() {
+        time=10;
+       if (!countingDown) {
+            timeKeep = setInterval(count, 1000);
+            countingDown = true;
+        }
+        
+    };
     
-    if (a2 === "BMW") {
-        correct++;
-    }
-    
-    if (a3 === "Peugeot") {
-        correct++;
-    }
-    
-    if (a4 === "Bugatti") {
-        correct++;
-    }
-    
-    console.log("correct: " + correct);
-    gameOver();
-});
+    function count() {
+         time--;
+         if (time === 0) {
+            $("#smallContainer").hide();
+            clockStop();
+            gameOver();
+        }
+        $("#clock").text(time);    
+    };
 
-//---------------GAMEOVER--------------------//
-var imageURL;
-
-function gameOver(){
-
-if (correct <= 1){
-    imageURL = "https://media.giphy.com/media/jUwpNzg9IcyrK/giphy.gif";
-}
-else if (correct <= 2){
-    imageURL = "https://media.giphy.com/media/9xijGdDIMovchalhxN/giphy.gif";
-}
-else if (correct <= 3){
-    imageURL = "https://media.giphy.com/media/Lcn0yF1RcLANG/giphy.gif";
-}
-else if (correct = 4){
-    imageURL = "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif";
-}
-
-    $("#allDone").text("ALL DONE!!");
-    $("#pointCounter").text("You got " + correct + " of the 10 questions correct.");
-    $("#doneImg").prepend($("<img>").attr("src", imageURL));
-    
-   
-};
-
-$("#start").on("click", function(){
-    $("#smallContainer").show();
-    $("#start").hide();
-})
-
-
-
-//----------------COUNTER----------------------//
-//Variables to hold values for countdown timer
-var timeKeep;
-var time = 11;
-var countingDown = false;
-$("#clock").text(time);
-
-
-//Function to start the clock 
-function clockStart() {
-    if (!countingDown) {
-        timeKeep = setInterval(count, 1000);
-        countingDown = true;
-    }
-
-};
-
-function count() {
-    time--;
-    if (time === 0) {
-        clockStop();
-        //gameOver();
-    }
-
-    $("#clock").text(time);
-};
-function clockStop() {
-    clearInterval(timeKeep);
-};
-//clockStart();
-
-$("#begin").on("click", clockStart());
-
+    function clockStop() {
+        clearInterval(timeKeep);
+        countingDown = false;
+    };
 });
